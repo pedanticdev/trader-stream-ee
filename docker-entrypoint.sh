ed7394243b4ecc8a476660709bfe8c5948a0a30e
Luqman Saeed <luqman.saeed@payara.fish>
2026-03-16 15:48:31 +0000

    Add needed modules for use of unsafe by aeron

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 # Default JVM options (without JFR)
 DEFAULT_OPTS="-Xms8g -Xmx8g \
--Xlog:gc*:file=/opt/payara/gc.log:time,uptime,level,tags:filecount=5,filesize=10M \
+-Xlog:gc*:file=/opt/payara/gc-logs/gc.log:time,uptime,level,tags:filecount=5,filesize=10M \
 -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions \
 -XX:+AlwaysPreTouch -XX:+UseTransparentHugePages \
--XX:-UseBiasedLocking -XX:+UseStringDeduplication \
+-XX:+UseStringDeduplication \
 -XX:+OptimizeStringConcat \
 -Djava.net.preferIPv4Stack=true \
 -Xlog:jfr*=info"
@@ -21,6 +21,9 @@ if [ "${JFR_ENABLED}" = "false" ]; then
 else
     # JFR enabled - add JFR options
     export JAVA_OPTS="${DEFAULT_OPTS} \
+--add-opens java.base/jdk.internal.misc=ALL-UNNAMED \
+--add-opens java.base/sun.nio.ch=ALL-UNNAMED \
+--add-opens java.base/java.nio=ALL-UNNAMED \
 -XX:StartFlightRecording=name=${RECORDING_NAME},filename=/opt/payara/recordings/recording.jfr,dumponexit=true,maxage=1h,maxsize=1g,method-profiling=normal \
 -XX:FlightRecorderOptions=stackdepth=256"
 fi
